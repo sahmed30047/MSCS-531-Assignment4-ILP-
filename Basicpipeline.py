f4098bfa0bd75856552921096819067e7e89205f
sahmed30047 <sahmed30047@ucumberlands.edu>
2024-10-27 18:18:16 -0400

    Update Basicpipeline.py

--- Basicpipeline.py
+++ Basicpipeline.py
@@ -1,87 +1,66 @@
+# config_no_branch_prediction.py
+
 import m5
 from m5.objects import *
+import os
 
 # Create the system
 system = System()
-system.clk_domain = SrcClockDomain(clock="1GHz", voltage_domain=VoltageDomain())
-system.mem_mode = "timing"
-system.mem_ranges = [AddrRange("512MB")]
+system.clk_domain = SrcClockDomain(clock='1GHz', voltage_domain=VoltageDomain())
+system.mem_mode = 'timing'
+system.mem_ranges = [AddrRange('512MB')]
 
-# Create CPU and Memory Bus
-system.cpu = O3CPU()
-# Branch predictor
-system.cpu.branchPred = LocalBP()
+# Create an in-order CPU (MinorCPU)
+system.cpu = MinorCPU()
 
-# Super scalar
-system.cpu.fetchWidth = 12
-system.cpu.issueWidth = 12
-system.cpu.dispatchWidth = 12
-system.cpu.commitWidth = 12
+# Disable branch prediction
+system.cpu.branchPred = NullBranchPredictor()
+
+# Create the L1 instruction and data caches
+icache = Cache(size='16kB', assoc=2)
+dcache = Cache(size='16kB', assoc=2)
+
+# Connect the caches to the CPU
+system.cpu.icache = icache
+system.cpu.dcache = dcache
+
+# Create a memory bus
 system.membus = SystemXBar()
 
-# Create a simple cache (4-way set associative)
-system.cpu.icache = Cache(
-    size='32kB',
-    assoc=4,  # 4-way set associative
-    tag_latency=2,
-    data_latency=2,
-    response_latency=1,
-    mshrs=16,
-    tgts_per_mshr=4
-)
-system.cpu.dcache = Cache(
-    size='32kB',
-    assoc=4,  # 4-way set associative
-    tag_latency=2,
-    data_latency=2,
-    response_latency=1,
-    mshrs=16,
-    tgts_per_mshr=4
-)
-
-# Connect caches to CPU and memory bus
-system.cpu.icache.cpu_side = system.cpu.icache_port  # Ensure this is correctly defined
+# Connect caches to the memory bus
 system.cpu.icache.mem_side = system.membus.cpu_side_ports
-
-system.cpu.dcache.cpu_side = system.cpu.dcache_port  # Ensure this is correctly defined
 system.cpu.dcache.mem_side = system.membus.cpu_side_ports
 
-# Set up interrupt controller
+# Connect CPU ports to caches
+system.cpu.icache.cpu_side = system.cpu.icache_port
+system.cpu.dcache.cpu_side = system.cpu.dcache_port
+
+# Create a memory controller
+system.mem_ctrl = DDR3_1600_8x8()
+system.mem_ctrl.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+# Set up the interrupt controller
 system.cpu.createInterruptController()
 system.cpu.interrupts[0].pio = system.membus.mem_side_ports
 system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
 system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
 
-# Create a memory controller
-system.mem_ctrl = MemCtrl()
-system.mem_ctrl.dram = DDR3_1600_8x8()
-system.mem_ctrl.dram.range = system.mem_ranges[0]
-system.mem_ctrl.port = system.membus.mem_side_ports
-
-
-# Create a process for a simple "Hello World" application
+# Set up the process to run (e.g., Dhrystone)
 process = Process()
-# Set the command
-# grab the specific path to the binary
-thispath = os.path.dirname(os.path.realpath(__file__))
-binpath = os.path.join(
-    thispath,
-    "../",
-    "tests/test-progs/hello/bin/x86/linux/hello",
-)
-# cmd is a list which begins with the executable (like argv)
-process.cmd = [binpath]
-# Set the cpu to use the process as its workload and create thread contexts
+binary = 'path/to/your/dhrystone/binary'  # Replace with the actual path
+process.cmd = [binary]
+
+# Assign the workload to the CPU
 system.cpu.workload = process
 system.cpu.createThreads()
 
-system.workload = SEWorkload.init_compatible(binpath)
-
-# set up the root SimObject and start the simulation
+# Instantiate the system
 root = Root(full_system=False, system=system)
-# instantiate all of the objects we've created above
 m5.instantiate()
 
-print(f"Beginning simulation!")
+print("Starting simulation without branch prediction...")
 exit_event = m5.simulate()
+
 print(f"Exiting @ tick {m5.curTick()} because {exit_event.getCause()}")
+
